3e13607993899783980a694517c951d12ef5eb4e
Fazekas Péter <Peter_Fazekas@epam.com>
2017-02-26 08:40:10 +0100

    refactor

--- src/main/java/hu/hotel/service/BookingService.java
+++ src/main/java/hu/hotel/service/BookingService.java
@@ -14,69 +14,61 @@ import java.util.Map;
  */
 public class BookingService {
 
-    private static final String NEWLINE = "\r\n";
-    private static final int MAXROOM = 27;
+    private static final String NEW_LINE = "\r\n";
+    private static final int MAXIMUM_NUMBER_OF_ROOMS = 27;
 
     private final List<Booking> bookings;
+    private boolean[] rooms = new boolean[MAXIMUM_NUMBER_OF_ROOMS];
 
     public BookingService(List<Booking> bookings) {
         this.bookings = bookings;
     }
 
     public String getLongestStay() {
-        Booking longestBooking = bookings.stream()
+        Booking longestStay = bookings.stream()
                 .max(Comparator.comparing(booking -> booking.getNumberOfNights()))
                 .get();
-        return longestBooking.getName() + " (" + longestBooking.getArrivalDay() + ") - " + longestBooking.getNumberOfNights();
+        return String.format("%s (%d) - %d", longestStay.getName(), longestStay.getArrivalDay(), longestStay.getNumberOfNights());
     }
 
     public String getTotalPrices() {
         StringBuilder sb = new StringBuilder();
-        bookings.forEach(booking -> sb.append(booking.getId() + ":" + booking.getTotalPrice() + NEWLINE));
+        bookings.forEach(i -> sb.append(String.format("%d:%d%s", i.getId(), i.getTotalPrice(), NEW_LINE)));
         return sb.toString();
     }
 
     public String getTotalGuestNights() {
         StringBuilder sb = new StringBuilder();
         Map<Month, Integer> totalGuestNights = calculateTotalGuestNights();
-        for (Map.Entry<Month, Integer> guestNights : totalGuestNights.entrySet()) {
-            int month = guestNights.getKey().getMonthId();
-            int guestNight = guestNights.getValue();
-            sb.append(NEWLINE + month + ": " + guestNight + " vendégéj");
-        }
+        totalGuestNights.entrySet()
+                .forEach(i -> sb.append(String.format("%s%d: %d vendégéj", NEW_LINE, i.getKey().getMonthId(), i.getValue())));
         return sb.toString();
     }
 
     private Map<Month, Integer> calculateTotalGuestNights() {
         Map<Month, Integer> totalGuestNights = new IntegerValueMap<>();
-        for (Booking booking : bookings) {
-            Map<Month, Integer> guestNights = booking.getGuestNights();
-            totalGuestNights.putAll(guestNights);
-        }
+        bookings.forEach(i -> totalGuestNights.putAll(i.getGuestNights()));
         return totalGuestNights;
     }
 
-    public String getFreeRooms(StayPeriod stayPeriod) {
-        boolean[] rooms = searchFreeRooms(stayPeriod);
+    public String getFreeRooms(final StayPeriod stayPeriod) {
+        searchFreeRooms(stayPeriod);
         int counter = 0;
-        for (int room = 0; room < rooms.length; room++) {
-            if(!rooms[room]) {
-                counter++;
-            }
+        for (boolean room : rooms) {
+            if (!room) counter++;
         }
-        return "A megadott időintervallumban " + counter + " szabad szoba áll rendelkezésre!";
+        return String.format("A megadott időintervallumban %d szabad szoba áll rendelkezésre!", counter);
     }
 
-    private boolean[] searchFreeRooms(StayPeriod stayPeriod) {
-        boolean[] rooms = new boolean[MAXROOM];
-        for (int day = stayPeriod.getArrivalDay(); day <= stayPeriod.getDepartureDay() ; day++) {
-            for (Booking booking : bookings) {
-                if(booking.isRoomOccupiedOnCertainDay(day)) {
-                    int room = booking.getRoom();
-                    rooms[room - 1] = true;
-                }
-            }
+    private void searchFreeRooms(StayPeriod stayPeriod) {
+        for (int day = stayPeriod.getArrivalDay(); day <= stayPeriod.getDepartureDay(); day++) {
+            setFreeRooms(day);
         }
-        return rooms;
+    }
+
+    private void setFreeRooms(final int day) {
+        bookings.stream()
+                .filter(i -> i.isRoomOccupiedOnCertainDay(day))
+                .map(i -> rooms[i.getRoom() - 1] = true);
     }
 }
