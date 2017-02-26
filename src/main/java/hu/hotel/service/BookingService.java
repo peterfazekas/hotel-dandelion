129d52ddbf7c47a60c2166ef1dc483c18e826634
Fazekas Péter <Peter_Fazekas@epam.com>
2017-02-26 16:28:07 +0100

    refactor

--- src/main/java/hu/hotel/service/BookingService.java
+++ src/main/java/hu/hotel/service/BookingService.java
@@ -20,23 +20,45 @@ public class BookingService {
     private final List<Booking> bookings;
     private boolean[] rooms = new boolean[MAXIMUM_NUMBER_OF_ROOMS];
 
+    /**
+     * Constructor.
+     * @param bookings - list of {@link Booking}
+     */
     public BookingService(List<Booking> bookings) {
         this.bookings = bookings;
     }
 
+    /**
+     * 2. feladat: Jelenítse meg a képernyőn a leghosszabb szállodai tartózkodást!
+     * Csak az időtartamot vegye figyelembe, azaz nem számít, hogy hány vendég lakott az adott szobában!
+     * Az esetlegesen azonos hosszúságú tartózkodások közül bármelyiket kiválaszthatja.
+     * @return String - a megfelelő válasz
+     */
     public String getLongestStay() {
         Booking longestStay = bookings.stream()
-                .max(Comparator.comparing(booking -> booking.getNumberOfNights()))
+                .max(Comparator.comparing(Booking::getNumberOfNights))
                 .get();
         return String.format("%s (%d) - %d", longestStay.getName(), longestStay.getArrivalDay(), longestStay.getNumberOfNights());
     }
 
+    /**
+     * 3. feladat: Számítsa ki, hogy az egyes foglalások után mennyit kell fizetnie az egyes vendégeknek!
+     * A foglalás sorszámát és a kiszámított értékeket kettősponttal elválasztva írja ki a bevetel.txt fájlba!
+     * @return String - a megfelelő válasz
+     */
     public String getTotalPrices() {
         StringBuilder sb = new StringBuilder();
         bookings.forEach(i -> sb.append(String.format("%d:%d%s", i.getId(), i.getTotalPrice(), NEW_LINE)));
         return sb.toString();
     }
 
+    /**
+     * 4. feladat: Készítsen statisztikát az egyes hónapokban eltöltött vendégéjszakákról!
+     * Egy vendégéjszakának egy fő egy eltöltött éjszakája számít.
+     * A példában szereplő Tóth család áprilisban 3, májusban pedig 9 vendégéjszakát töltött a szállodában.
+     * Írassa ki a havi vendégéjszakák számát a képernyőre.
+     * @return String - a megfelelő válasz
+     */
     public String getTotalGuestNights() {
         StringBuilder sb = new StringBuilder();
         Map<Month, Integer> totalGuestNights = calculateTotalGuestNights();
@@ -51,6 +73,13 @@ public class BookingService {
         return totalGuestNights;
     }
 
+    /**
+     * 5. feladat: Kérje be a felhasználótól egy új foglalás kezdő dátumához tartozó nap sorszámát és az
+     * eltöltendő éjszakák számát! Határozza meg, hogy hány szoba szabad a megadott időszak teljes időtartamában!
+     * A választ írassa ki a képernyőre!
+     * @param stayPeriod as {@link StayPeriod} - foglalási idő tól - ig.
+     * @return String - a megfelelő válasz
+     */
     public String getFreeRooms(final StayPeriod stayPeriod) {
         searchFreeRooms(stayPeriod);
         int counter = 0;
@@ -69,6 +98,6 @@ public class BookingService {
     private void setFreeRooms(final int day) {
         bookings.stream()
                 .filter(i -> i.isRoomOccupiedOnCertainDay(day))
-                .map(i -> rooms[i.getRoom() - 1] = true);
+                .forEach(i -> rooms[i.getRoom() - 1] = true);
     }
 }
